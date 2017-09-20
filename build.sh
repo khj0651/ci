112736d411e550f7d1e8bdb0e6c2459d35a6dbd3
khj0651 <khj0651@gmail.com>
2017-09-20 13:33:40 +0900

    build

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+cd paasta-monitoring-management
+
+export GOPATH=$PWD
+export PATH=$GOPATH/bin:$PATH
+
+cd src/kr/paasta/monitoring
+
+#go get github.com/cloudfoundry-incubator/runtime-schema
+go get github.com/tedsuo/ifrit
+
+#rata
+go get github.com/tedsuo/rata
+
+#Mysql Driver and Orm Library Download
+go get github.com/go-sql-driver/mysql
+go get github.com/jinzhu/gorm
+
+#InfluxDB Library Download
+go get github.com/influxdata/influxdb/client/v2
+
+
+#TestCode
+go get github.com/gorilla/handlers
+go get github.com/gorilla/mux
+go get github.com/stretchr/testify/assert
+go get github.com/onsi/ginkgo
+go get github.com/onsi/gomega
+
+#godep
+go get github.com/tools/godep
+go get golang.org/x/sys/unix
+go get github.com/davecgh/go-spew/spew
+go get github.com/pmezard/go-difflib/difflib
+
+
+go build main.go
